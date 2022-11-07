acad9b8277dbe8b4f229949f2c94b43b774a96fc
Iswar <rick731202@gmail.com>
2022-11-07 14:49:40 +0530

    Some changes to the routes for compatibility with frontend

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const User = require('./models/User');
 const jwt = require('jsonwebtoken');
 const auth = require('./middlewares/auth');
 require('dotenv').config()
+const cors = require("cors");
 
 mongoose.connect('mongodb://localhost:27017/urlShortener');
 
@@ -14,6 +15,7 @@ app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
+app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 
@@ -22,11 +24,17 @@ app.get('/', async (req, res) => {
     res.render('index', {allShortURL});
 })
 
+app.get('/get-all-urls', async (req, res) => {
+    const allShortURL = await ShortURL.find();
+    res.status(200).json({success: true, message:"Successfully retrieved all URLs.", data: allShortURL});
+})
+
 
 app.post('/short-that-url', async (req, res) => {
     console.log(req.body);
     await ShortURL.create({ fullURL: req.body.fullURL });
-    res.redirect('/');
+    // res.redirect('/');
+    res.status(200).json({success: true, message: "URL created successfully", error: false});
 })
 
 app.get('/:shortURL', async (req, res)=>{
@@ -54,14 +62,16 @@ app.post('/api/user/register', async (req, res) =>{
         const hash = await bcrypt.hash(`${password}`, 10); // converted password into a string
 
         const user = new User({
-            username,
+            username: username.toString(),
             email,
             password: hash
         })
         try{
             const saveUser = await user.save();
-            res.status(200).send(saveUser);
+            res.status(200).json({ success: true, message: "New user added", username: saveUser.username})
         }catch(err){
+            console.log(user);
+            console.log(err);
             res.status(400).send({"message": err.message});
         }
     }
